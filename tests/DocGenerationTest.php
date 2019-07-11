9a8629e3141ff9ddf68534a48bbf054052e613a0
abbadon1334 <fdanti@gmail.com>
2019-07-11 13:15:52 +0300

    create real test + raise coverage

---
+++ tests/DocGenerationTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace JuliusHaertl\PHPDocToRst\Test;
+
+use JuliusHaertl\PHPDocToRst\ApiDocBuilder;
+use JuliusHaertl\PHPDocToRst\Extension\GithubLocationExtension;
+use JuliusHaertl\PHPDocToRst\Extension\InterfaceImplementors;
+use JuliusHaertl\PHPDocToRst\Extension\NoPrivateExtension;
+use JuliusHaertl\PHPDocToRst\Extension\PublicOnlyExtension;
+use JuliusHaertl\PHPDocToRst\Extension\TocExtension;
+use PHPUnit_Framework_TestCase;
+
+class DocGenerationTest extends PHPUnit_Framework_TestCase
+{
+    public function testBuildDocumentation()
+    {
+
+        $src = [__DIR__ . '/../src'];
+        $dst = __DIR__ . '/../docs/api';
+
+        $apiDocBuilder = new ApiDocBuilder($src, $dst);
+        $apiDocBuilder->setVerboseOutput(true);
+        $apiDocBuilder->setDebugOutput(true);
+        $apiDocBuilder->addExtension(InterfaceImplementors::class);
+        $apiDocBuilder->build();
+    }
+
+    public function testDocumentingSelf()
+    {
+
+        $src = [__DIR__ . '/../src'];
+        $dst = __DIR__ . '/../tests/self-doc';
+
+        $apiDocBuilder = new ApiDocBuilder($src, $dst);
+        $apiDocBuilder->setVerboseOutput(true);
+        $apiDocBuilder->setDebugOutput(true);
+        $apiDocBuilder->addExtension(InterfaceImplementors::class);
+        $apiDocBuilder->build();
+    }
+
+    public function testDocumentingPSR()
+    {
+
+        $src = [__DIR__ . '/../vendor/psr'];
+        $dst = __DIR__ . '/../tests/psr-doc';
+
+        $apiDocBuilder = new ApiDocBuilder($src, $dst);
+        $apiDocBuilder->setVerboseOutput(true);
+        $apiDocBuilder->setDebugOutput(true);
+        $apiDocBuilder->addExtension(InterfaceImplementors::class);
+        $apiDocBuilder->build();
+    }
+
+    public function testDocumentingDocumentor()
+    {
+        $src = [__DIR__ . '/../vendor/phpdocumentor'];
+        $dst = __DIR__ . '/../tests/documentor-doc';
+
+        $apiDocBuilder = new ApiDocBuilder($src, $dst);
+        $apiDocBuilder->setVerboseOutput(true);
+        $apiDocBuilder->setDebugOutput(true);
+        $apiDocBuilder->addExtension(InterfaceImplementors::class);
+        $apiDocBuilder->build();
+    }
+
+    public function testDocumentingWithExtension()
+    {
+        $src = [__DIR__ . '/../src'];
+        $dst = __DIR__ . '/../tests/self-doc';
+
+        $apiDocBuilder = new ApiDocBuilder($src, $dst);
+        $apiDocBuilder->setVerboseOutput(true);
+        $apiDocBuilder->setDebugOutput(true);
+        $apiDocBuilder->addExtension(PublicOnlyExtension::class);
+        $apiDocBuilder->addExtension(NoPrivateExtension::class);
+        $apiDocBuilder->addExtension(TocExtension::class);
+        $apiDocBuilder->addExtension(InterfaceImplementors::class);
+        $apiDocBuilder->addExtension(GithubLocationExtension::class, [
+            'http://github.com/abbadon1334',
+            'phpdoc-to-rst',
+            'master'
+        ]);
+        $apiDocBuilder->build();
+    }
+}
